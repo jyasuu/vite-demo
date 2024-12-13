5f296a29e3bef4f24474c12f1305d11c62223e70
jyasuu <s1002605@pu.edu.tw>
2024-12-13 06:44:37 +0000

    real profile data

--- src/views/Introduction.vue
+++ src/views/Introduction.vue
@@ -1,35 +1,36 @@
 <template>
-    <div class="introduction container mx-auto p-6 bg-white shadow-md rounded-lg">
-      <h1 class="text-3xl font-bold mb-6 text-center text-blue-600">Your Name</h1>
-      <section>
-        <h2 class="text-2xl font-semibold mb-4 text-blue-500">Professional Background Summary:</h2>
-        <ul class="list-disc list-inside">
-          <li class="mb-2">~7 years as Front End Developer in E-Commerce, IT Consultant, Computer Vision and Cyber Security industries</li>
-          <li class="mb-2">Proficient in JavaScript frameworks, incl. Vue and React</li>
-          <li class="mb-2">I have high ownership about products I develop</li>
-          <li class="mb-2">Worked with multinational team with dynamic changes and tight deadlines</li>
-          <li class="mb-2">I have strong CSS skill, always make pixel perfect design</li>
-          <li class="mb-2">Experienced mentoring frontend or future tech talents</li>
-        </ul>
-      </section>
-      <section class="mt-6">
-        <h2 class="text-2xl font-semibold mb-4 text-blue-500">Educational Background:</h2>
-        <ul class="list-disc list-inside">
-          <li class="mb-2">Bachelor of Engineering in Computer & Information Sciences from Universitas Katolik Parahyangan</li>
-        </ul>
-      </section>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    name: 'Introduction',
-  };
-  </script>
-  
-  <style scoped>
-  .introduction {
-    margin: 20px;
-  }
-  </style>
-  
+  <div class="introduction container mx-auto p-6 bg-white shadow-md rounded-lg">
+    <h1 class="text-3xl font-bold mb-6 text-center text-blue-600">Zi-Xuan Yen</h1>
+    <section>
+      <h2 class="text-2xl font-semibold mb-4 text-blue-500">Professional Background Summary:</h2>
+      <ul class="list-disc list-inside">
+        <li>Fullstack Engineer with a Bachelor's degree in Computer Science from Providence University and over five years of experience at Pouchen.</li>
+        <li>Experience with full-cycle system development, architecture design, and CI/CD implementation.</li>
+        <li>Successfully led projects like a self-hosted GitLab environment, Grafana/Prometheus monitoring systems, and automation tools to improve development workflows.</li>
+        <li>Developed a TV ad scheduling system using C# .NET WPF, researched computer vision for attendance management, and built a web-based system using C# ASP.NET MVC and AngularJS.</li>
+        <li>Technical expertise includes Java, C#, Docker, AWS, GitLab CI/CD, RabbitMQ, Postgres, and more.</li>
+        <li>Passionate about continuous learning, exploring new technologies through side projects, and enjoy mentoring colleagues.</li>
+      </ul>
+    </section>
+    <section class="mt-6">
+      <h2 class="text-2xl font-semibold mb-4 text-blue-500">Educational Background:</h2>
+      <ul class="list-disc list-inside">
+        <li>Bachelor of Engineering in Computer Science Information Engineering from Providence University.</li>
+        <li>Participated in the ACM programming competition.</li>
+        <li>Developed music recognition and notation program.</li>
+      </ul>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Introduction',
+};
+</script>
+
+<style scoped>
+.introduction {
+  margin: 20px;
+}
+</style>
--- src/views/MilestoneSlide.vue
+++ src/views/MilestoneSlide.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="max-w-4xl mx-auto p-6 bg-white shadow-md rounded-lg">
       <h2 class="text-2xl font-bold mb-4">{{ milestone.title }}</h2>
-      <p class="mb-2"><strong>Purpose:</strong> {{ milestone.purpose }}</p>
-      <p class="mb-2"><strong>Technology used:</strong> {{ milestone.technology }}</p>
-      <p class="mb-2"><strong>Duration:</strong> {{ milestone.duration }}</p>
+      <p class="mb-2"><strong>Situation:</strong> {{ milestone.situation }}</p>
+      <p class="mb-2"><strong>Task:</strong> {{ milestone.task }}</p>
+      <p class="mb-2"><strong>Action:</strong> {{ milestone.action }}</p>
       <p class="mb-2"><strong>Result & Outcome:</strong> {{ milestone.result }}</p>
       <img :src="milestone.image" alt="Milestone Image" class="mt-4 max-w-full h-auto rounded-md">
     </div>
@@ -22,14 +22,58 @@
       const milestones = [
         {
           id: 1,
-          title: 'Slot for Ads',
-          purpose: 'Create slotting time for ads in Tokopedia based on Group Campaign, Campaign, Package, Inventory, and Affinity Category.',
-          technology: 'React',
-          duration: '6 months',
-          result: 'Increased revenue and reduced manpower needed and time. Optimized performance by maintaining consistent DOM length.',
-          image: '/src/assets/midestone-slide.png'
+          title: 'Oracle to Postgres at Pouchen',
+          situation: 'For ISO 27001 and Oracle license costs.',
+          task: 'Responsible for developing the system to be compatible with Oracle and Postgres.',
+          action: 'Developed the system modify sql to standard between database for compatibility.Use the ora2pg tool for data migration.',
+          result: 'Migration success and optimized resource usage.',
+          image:'/src/assets/midestone-slide.png', // Replace with actual image path
         },
-        // Add more milestones as needed
+        {
+          id: 2,
+          title: 'Automation of Development and Deployment at Pouchen',
+          situation: 'Optimise automate development and deployment.',
+          task: 'Built an automated environment.',
+          action: 'Set up a self-hosted GitLab, integrated CI/CD tools, and implemented Ansible for automation.',
+          result: 'Increased deployment speed, reduced errors, and boosted productivity.',
+          image: '/src/assets/midestone-slide.png', // Replace with actual image path
+        },
+        {
+          id: 3,
+          title: 'Supplier Chain System at Pouchen',
+          situation: 'For supplier chain support include B2B ,Bidding.',
+          task: 'Designed the system architecture.',
+          action: 'Implemented the architecture on AWS, ensuring scalability , security and reliability.',
+          result: 'The system supported the company’s B2B business needs.',
+          image: '/src/assets/midestone-slide.png', // Replace with actual image path
+        },
+        {
+          id: 4,
+          title: 'Service Optimization for Performance and Cost Efficiency at Pouchen',
+          situation: 'Services needed optimization for better performance and cost efficiency.',
+          task: 'Improved performance and reduced costs.',
+          action: 'Analyzed and optimized key system components for resource efficiency.',
+          result: 'Reduced storage costs and improved system performance.',
+          image: '/src/assets/midestone-slide.png', // Replace with actual image path
+        },
+        {
+          id: 5,
+          title: 'TV Ad Scheduling System at Sinyentech',
+          situation: 'The customer required a system for managing TV ad schedules.',
+          task: 'Develop and maintain the TV ad scheduling system.',
+          action: 'Created the system using C# .NET WPF, focusing on an interface for scheduling and updating ads. Ensured alignment with business requirements and scalability for future needs.',
+          result: 'The system improved scheduling efficiency, reduced broadcast errors, and enhanced overall customer satisfaction.',
+          image: '/src/assets/midestone-slide.png', // Replace with actual image path
+        },
+        {
+          id: 6,
+          title: 'Sales and Accounting System at Sinyentech',
+          situation: 'The customer sales and accounting system was outdated and inefficient.',
+          task: 'Develop a modern, web-based solution to streamline these functions.',
+          action: 'Using C# ASP.NET MVC and AngularJS, developed a responsive interface, integrated real-time sales data, and collaborated with business teams.',
+          result: 'The new system improved transaction speed and enhanced operational efficiency.',
+          image: '/src/assets/midestone-slide.png', // Replace with actual image path
+        },
       ];
       this.milestone = milestones.find(m => m.id === parseInt(id));
     },
